3e54e28c07601f0dab0f63cfe0e20ab56905ae62
PawanKumar1216 <32115892@student.uwl.ac.uk>
2026-05-04 14:59:45 +0100

    Create mongodb_queries.js

---
+++ mongodb/mongodb_queries.js
@@ -0,0 +1,9 @@
+// NorthStar Urban Mobility and Logistics
+// MongoDB query file
+
+// This file will contain MongoDB Atlas queries,
+// collection operations, filtering, aggregation,
+// and optimisation examples.
+
+// Example placeholder query:
+db.trips.find({});
