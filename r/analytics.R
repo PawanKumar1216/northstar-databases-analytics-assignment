dde926d8146ec25193a09ce537c711872057c927
PawanKumar1216 <32115892@student.uwl.ac.uk>
2026-05-04 14:58:55 +0100

    Create analytics.R

---
+++ r/analytics.R
@@ -0,0 +1,11 @@
+# NorthStar Urban Mobility and Logistics
+# R analytics file
+
+# This file will contain descriptive statistics,
+# visual analysis, and coursework analytics tasks.
+
+main <- function() {
+  print("NorthStar R analytics script")
+}
+
+main()
